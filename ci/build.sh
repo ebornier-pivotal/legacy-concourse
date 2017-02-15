f235796a927a19bc726771da92691193a1367417
erwanbornier <ebornier@pivotal.io>
2017-02-15 23:55:42 +0100

    commit

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cd legacy-concourse-repo
-apt-get install --force-yes golang
+apt-get install golang --force-yes
 go build app.go
 #./mvnw clean package -Dmaven.test.skip=true 
 #mv target/*.jar ../build/
